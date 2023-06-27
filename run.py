b791b1d277de08c1fa8c47475bb9df01561276a7
Sayantan Bose <75779600+Sayantan19@users.noreply.github.com>
2023-06-27 11:17:00 +0530

    Fixed changes

--- run.py
+++ run.py
@@ -5,36 +5,36 @@ from waitress import serve
 
 app = Flask(__name__)
 openai.api_type = "azure"
-openai.api_base = os.getenv('ENDPOINT')
+openai.api_base = os.getenv("ENDPOINT")
 openai.api_version = "2023-03-15-preview"
-# openai.api_version = "2022-12-01"
 openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_version = "2022-12-01"
 
 
 @app.route("/chatres", methods=["POST"])
 def hello_world():
     if(request.method == 'POST'):
-        print(request.json)
-        return 'very good'
-    # response = openai.ChatCompletion.create(
-    #     engine="ChatSpeaks",
-    #     messages=[
-    #         {
-    #             "role": "system",
-    #             "content": "You are an AI assistant that helps people find information."
-    #         }
-    #     ],
-    #     temperature=0.7,
-    #     max_tokens=800,
-    #     top_p=0.95,
-    #     frequency_penalty=0,
-    #     presence_penalty=0,
-    #     stop=None)
-    # print(response)
-    return 'good'
+        print(request.json['message'])
+        response = openai.ChatCompletion.create(
+            engine="ChatSpeaks",
+            messages=[
+                {
+                    "role": "system",
+                    "content": request.json['message']
+                }
+            ],
+            temperature=0.7,
+            max_tokens=800,
+            top_p=0.95,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None)
+        print(response)
+        return response["choices"]
+    return "good"
 
 
 if (__name__ == '__main__'):
-    # app.run(debug=True, port=8000, host="0.0.0.0")
+    # app.run(debug=True, port=15000, host="0.0.0.0")
     print("Waitress Running")
-    serve(app, host='0.0.0.0',port=15000)
+    # serve(app, host='0.0.0.0',port=15000)
